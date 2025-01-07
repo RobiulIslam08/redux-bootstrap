a9b1e07bbd75f3d00a3920757bda6a9cc67cca7b
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-01-07 12:31:11 +0600

    Managing Task Completion and Deletion

--- src/components/module/task/TaskCard.tsx
+++ src/components/module/task/TaskCard.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
+import { deleteTask, toggleCompleteState } from "@/redux/features/task/taskSlice";
+import { useAppDispatch } from "@/redux/hook";
 import { ITask } from "@/types";
 
 import { Trash2 } from "lucide-react";
@@ -8,6 +10,7 @@ interface IProps {
   task:ITask
 }
 export default function TaskCard({task}:IProps) {
+  const dispatch =useAppDispatch()
   return (
     <div className="border px-5 py-3 rounded-md">
       <div className="flex justify-between items-center">
@@ -17,13 +20,13 @@ export default function TaskCard({task}:IProps) {
             "bg-red-500":task.priority === "low",
             "bg-blue-500":task.priority === "medium",
           })}></div>
-          <h1>{task.title}</h1>
+          <h1 className={cn({"line-through":task.isCompleted})}>{task.title}</h1>
         </div>
         <div className="flex gap-3 items-center">
-          <Button variant="link" className="p-0 text-red-500">
+          <Button onClick={()=>dispatch(deleteTask(task?.id))} variant="link" className="p-0 text-red-500">
             <Trash2 />
           </Button>
-          <Checkbox />
+          <Checkbox checked={task.isCompleted} onClick={()=>dispatch(toggleCompleteState(task?.id))}/>
         </div>
       </div>
       <p className="mt-5">{task.description}</p>
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -20,11 +20,19 @@ const taskSlice = createSlice({
     addTask:(state, action:PayloadAction<DraftData>) => {
      const taskData = createTask(action.payload)
       state.tasks.push(taskData)
+    },
+    toggleCompleteState: (state,action:PayloadAction<string>) => {
+      console.log(action.payload)
+        state.tasks.forEach(task => task.id === action.payload ? task.isCompleted = !task.isCompleted: task)
+    },
+    deleteTask: (state,action:PayloadAction<string>) => {
+      console.log(action.payload)
+       state.tasks = state.tasks.filter(task => task.id !== action.payload)
     }
   },
 });
 export const selectTask = (state: RootState) => {
   return state.todo.tasks;
 };
-export const {addTask} = taskSlice.actions
+export const {addTask,toggleCompleteState, deleteTask} = taskSlice.actions
 export default taskSlice.reducer;
